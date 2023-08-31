c0c247ce1c24b492ba4c88d36e1bbab3979c5a28
SherlyAmorim <sherlyamorim@gmail.com>
2023-08-31 18:16:59 -0300

    :test_tube: Test: Teams route coverage increase

--- app/backend/src/tests/mocks/Teams.mocks.ts
+++ app/backend/src/tests/mocks/Teams.mocks.ts
@@ -3,9 +3,15 @@ const team = {
   teamName: 'Avaí/Kindermann',
 };
 
+const teamNotFound = [{
+  id: 1,
+  teamName: 'Avaí/Kindermann',
+}];
+
 const teams = [team];
 
 export {
   team,
   teams,
+  teamNotFound,
 };
--- app/backend/src/tests/teams.test.ts
+++ app/backend/src/tests/teams.test.ts
@@ -6,7 +6,7 @@ import chaiHttp = require('chai-http');
 
 import { App } from '../app';
 import Teams from '../database/models/TeamsSequelize';
-import { team, teams } from './mocks/Teams.mocks';
+import { team, teamNotFound, teams } from './mocks/Teams.mocks';
 
 chai.use(chaiHttp);
 
@@ -15,6 +15,8 @@ const { app } = new App();
 const { expect } = chai;
 
 describe('Teams teste', function() {
+  beforeEach(function () {sinon.restore(); });
+
   it('Get Teams Route', async function () {
     sinon.stub(Teams, 'findAll').resolves(teams as any);
 
@@ -24,13 +26,39 @@ describe('Teams teste', function() {
     expect(body).to.deep.equal(teams);
   })
 
+  it('Get Teams Route Error 500', async function () {
+    const error = new Error('Internal Server Error');
+    sinon.stub(Teams, 'findAll').throws(error);
+
+    const res = await chai.request(app).get('/teams');
+        
+    expect(res).to.have.status(500);
+  })
+  
   it('Get Teams/id Route', async function () {
     sinon.stub(Teams, 'findByPk').resolves(team as any);
-
+    
     const {status, body} = await chai.request(app).get('/teams/1');
-
+    
     expect(status).to.equal(200);
     expect(body).to.deep.equal(team);
   })
 
+  it('Get Teams/id Not Found', async function () {
+    sinon.stub(Teams, 'findByPk').resolves(teamNotFound[5] as any);
+        
+    const res = await chai.request(app).get('/teams/5');
+    
+    expect(res.status).to.equal(404);
+  })
+  
+  it('Get Teams/id Route Error 500', async function () {
+    const error = new Error('Internal Server Error');
+    sinon.stub(Teams, 'findByPk').throws(error);
+
+    const res = await chai.request(app).get('/teams/1');
+        
+    expect(res).to.have.status(500);
+  })
 });
+
