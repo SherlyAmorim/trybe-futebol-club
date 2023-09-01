bf73852713e5b602b13e977b96b3439c9fdc6b60
SherlyAmorim <sherlyamorim@gmail.com>
2023-08-31 23:06:05 -0300

    :sparkles: Feat: Created validations for email and password

--- app/backend/src/middlewares/UsersValidations.ts
+++ app/backend/src/middlewares/UsersValidations.ts
@@ -6,6 +6,16 @@ export default class UsersValidations {
     if (!email || !password) {
       return res.status(400).json({ message: 'All fields must be filled' });
     }
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+      return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
+    if (password.length < 6) {
+      return res.status(401).json({ message: 'Invalid email or password' });
+    }
+
     next();
   }
 }
