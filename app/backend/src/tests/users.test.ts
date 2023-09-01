9b1e10a67dbba704f11ccf043e8dd891df98cc71
SherlyAmorim <sherlyamorim@gmail.com>
2023-08-31 23:04:18 -0300

    :test_tube: Test: TDD Login User to increase coverage

--- app/backend/src/tests/users.test.ts
+++ app/backend/src/tests/users.test.ts
@@ -6,8 +6,9 @@ import chaiHttp = require('chai-http');
 
 import { App } from '../app';
 import Users from '../database/models/UsersSequelize';
-import { login, noEmailLoginBody, noPasswordLoginBody, rash, token } from './mocks/Users.mocks';
+import { login, noEmailLoginBody, noPasswordLoginBody, rash, token, validLoginBody } from './mocks/Users.mocks';
 import UsersValidations from '../middlewares/UsersValidations';
+import JWT from '../utils/JWT';
 
 chai.use(chaiHttp);
 
@@ -37,13 +38,34 @@ describe('Users teste', function() {
     expect(result.status).to.equal(400);
     expect(result.body).to.deep.equal({ "message": "All fields must be filled" });
   })
-
+  
+  it('Post Users Route with invalid email or password', async function () {
+    sinon.stub(Users, 'findOne').resolves(null);
+    sinon.stub(UsersValidations, 'validateLogin').returns();
+    
+    const resp = await chai.request(app).post('/login').send( { email: 'sherly@admin.com', password: 'secret_admin' });
+    
+    expect(resp.status).to.equal(401);
+    expect(resp.body).to.deep.equal({ message: 'Invalid email or password' });
+  })
+  
+  // it('Post Users Route with invalid email or password', async function () {
+    //   sinon.stub(Users, 'findOne').resolves(validLoginBody[0] as any);
+    
+    //   const resp = await chai.request(app).post('/login').send( { email: 'sherly@admin.com', password: 'secret_admin' });
+    
+    //   expect(resp.status).to.equal(401);
+    //   expect(resp.body).to.deep.equal({ message: 'Invalid email or password' });
+    // })
+    
   it('Post Users Route', async function () {
     sinon.stub(Users, 'findOne').resolves(rash as any);
     sinon.stub(UsersValidations, 'validateLogin').returns();
 
-    const { status } = await chai.request(app).post('/login').send(login);
-
-    expect(status).to.equal(200);
+    const resp = await chai.request(app).post('/login').send(login);
+    
+    expect(resp.status).to.equal(200);
+    expect(resp.body).to.have.key('token');
+    expect(resp.body.token).to.be.a('string');
   })
 });
