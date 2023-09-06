90c00096a518466b01ef71df19c8e8f5942f2a61
SherlyAmorim <sherlyamorim@gmail.com>
2023-09-06 15:24:34 -0300

    :recycle: Refactor: Add mapStatus funct. in userValidation

--- app/backend/src/middlewares/UsersValidations.ts
+++ app/backend/src/middlewares/UsersValidations.ts
@@ -1,19 +1,20 @@
 import { NextFunction, Request, Response } from 'express';
+import mapStatus from '../utils/mapStatus';
 
 export default class UsersValidations {
   static validateLogin(req:Request, res: Response, next: NextFunction): Response | void {
     const { email, password } = req.body;
     if (!email || !password) {
-      return res.status(400).json({ message: 'All fields must be filled' });
+      return res.status(mapStatus('INVALID_DATA')).json({ message: 'All fields must be filled' });
     }
 
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
-      return res.status(401).json({ message: 'Invalid email or password' });
+      return res.status(mapStatus('UNAUTHORIZED')).json({ message: 'Invalid email or password' });
     }
 
     if (password.length < 6) {
-      return res.status(401).json({ message: 'Invalid email or password' });
+      return res.status(mapStatus('UNAUTHORIZED')).json({ message: 'Invalid email or password' });
     }
 
     next();
