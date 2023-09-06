354b7365ecfc7f3f9a88434084037499de7c46ef
SherlyAmorim <sherlyamorim@gmail.com>
2023-09-06 17:10:05 -0300

    :recycle: Refactor: Changed parameter type

--- app/backend/src/models/Users.model.ts
+++ app/backend/src/models/Users.model.ts
@@ -5,14 +5,14 @@ import UsersSequelize from '../database/models/UsersSequelize';
 export default class UsersModel implements IUserModel {
   private model = UsersSequelize;
 
-  async loginUser(email: string): Promise<IUser | null> {
+  async loginUser(email: IUser['email']): Promise<IUser | null> {
     const dbData = await this.model.findOne({ where: { email } });
     if (!dbData) return null;
     const { id, username, password } = dbData;
     return { id, username, password, email };
   }
 
-  async getRole(email: string): Promise<IUser['role'] | null> {
+  async getRole(email: IUser['email']): Promise<IUser['role'] | null> {
     const dbData = await this.model.findOne({ where: { email } });
     if (!dbData) return null;
     const { role } = dbData;
